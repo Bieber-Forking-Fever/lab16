69e6dc46529423706185f46448a15487392634b3
Bieber-Forking-Fever <asfiohqwtgpowh@gmail.com>
2014-11-13 15:57:16 -0600

    aqoiwfghwoiehwa

--- 70-100pt.py
+++ 70-100pt.py
@@ -4,15 +4,16 @@
 # 90pt -  When you hit space, fire a missile straight up! 
 #         Subtract from how many missiles you have left
 # 100pt - Destroy the target if a missile hits it! 
-# Hints: use drawpad.destroy(enemy) in the collision detect function, which you can trigger
+# Hints: use drawpad.delete(enemy) in the collision detect function, which you can trigger
 # from the key press event... maybe a loop to keep checking until the rocket goes out of bounds?
 from Tkinter import *
 root = Tk()
 drawpad = Canvas(root, width=800,height=600, background='white')
 player = drawpad.create_oval(390,580,410,600, fill="blue")
 enemy = drawpad.create_rectangle(50,50,100,60, fill="red")
-
+rocket = drawpad.create_oval(395,590,405,580, fill="red")
 direction = 5
+rocketFired = False
 
 class myApp(object):
     def __init__(self, parent):
@@ -43,29 +44,88 @@ class myApp(object):
         global drawpad
         global enemy
         global direction
+        global rocketFired
         x1,y1,x2,y2 = drawpad.coords(enemy)
         if x2 > 800:
             direction = - 5
         elif x1 < 0:
             direction = 5
         drawpad.move(enemy, direction, 0)
-        drawpad.after(5,self.animate)
+        
+        if rocketFired == True:
+            drawpad.move(rocket,0,-5)
+            didWeHit = self.collisionDetect()
+            if didWeHit:
+                print "hello"
+                drawpad.delete(enemy) 
 
-    def key(self,event):
-        global player
-        if event.char == "w":
-            drawpad.move(player,0,-15)
-        if event.char == "a":
-            drawpad.move(player,-15,0)
-        if event.char == "s":
-            drawpad.move(player,0,15)
-        if event.char == "d":
-            drawpad.move(player,15,0)
+        x1,y1,x2,y2 = drawpad.coords(player)
+        rx1,ry1,rx2,ry2 = drawpad.coords(rocket)
+        if ry1 <= 0 :
+            rocketFired = False
+            drawpad.move(rocket,(x1-rx1),(y1-ry1))
+            
+        drawpad.after(1,self.animate)
+
+    def collisionDetect(self):
+                global target
+		global drawpad
+                global player
+                global enemy
+                x1, y1, x2, y2 = drawpad.coords(enemy)
+                rx1,ry1,rx2,ry2 = drawpad.coords(rocket)
+                if (rx1 > x1 and rx2 < x2) and (ry1 > y1 and ry2 < y2):
+                    print "true"
+                    return True
+                else:
+                    return False
+
+
+
+
+
+
+
+
+
 
 
         
-    def collisionDetect(self,rocket):
-        rx1,ry1,rx2,ry2 = drawpad.coords(rocket)
+
+    def key(self,event):
+        global player
+        global rocketFired
+        if event.char == "w":
+            drawpad.move(player,0,-15)
+            drawpad.move(rocket,0,-15)
+        if event.char == "a":
+            drawpad.move(player,-15,0)
+            drawpad.move(rocket,-15,0)
+        if event.char == "s":
+            drawpad.move(player,0,15)
+            drawpad.move(rocket,0,15)
+        if event.char == "d":
+            drawpad.move(player,15,0)
+            drawpad.move(rocket,15,0)
+        if event.char == " ":
+            rocketFired = True
+            self.rockets = self.rockets - 1
+            self.rocketsTxt.config(text=str(self.rockets))
+
+
+
+
+ 
+            
+
+            
+            
+
+        
+
+
+
+
         
 
 app = myApp(root)
